352209769228074c391d56ace3319c62442af8d9
luis2388 <agstartdigital1@gmail.com>
2023-08-03 02:47:39 -0300

    add folder build

---
+++ frontend/build/precache-manifest.b194739f851bebec30d076318d7833c3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26268aec568aa4a039877e9da5bfef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c48c9e2da05460d783d0",
+    "url": "/static/css/2.fa426b8c.chunk.css"
+  },
+  {
+    "revision": "c48c9e2da05460d783d0",
+    "url": "/static/js/2.560aae1b.chunk.js"
+  },
+  {
+    "revision": "18b8f98f0165ce15867e2ed560084bb5",
+    "url": "/static/js/2.560aae1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984fdd1e5c945c4695f3",
+    "url": "/static/js/3.f3951e33.chunk.js"
+  },
+  {
+    "revision": "c15eddeb09b46511cf0731116c7067b0",
+    "url": "/static/js/3.f3951e33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06cc11b08ef3ffc80c20",
+    "url": "/static/js/main.b4433b9d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.b4433b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4f159c33905355b32d",
+    "url": "/static/js/runtime-main.d2275a43.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "2ad2d2a490faf8912451e5374a482ef5",
+    "url": "/static/media/logo.2ad2d2a4.png"
+  },
+  {
+    "revision": "2ad2d2a490faf8912451e5374a482ef5",
+    "url": "/static/media/logologin.2ad2d2a4.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "9fd5812c1c2c974cc29e53e138745809",
+    "url": "/static/media/sound.9fd5812c.mp3"
+  },
+  {
+    "revision": "2675a05168b3a218703c6d72bb336e02",
+    "url": "/static/media/wa-background.2675a051.png"
+  }
+]);
